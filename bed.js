526ab8faf32c2a2262603b7c3987eb064c5a37b4
Emma Remon <90062286+ecs3edec3ex232@users.noreply.github.com>
2022-12-10 17:04:58 -0500

    Add files via upload

---
+++ bed.js
@@ -0,0 +1,37 @@
+img ="";
+status ="";
+
+function back(){
+    window.location ="index.html";
+}
+
+function preload(){
+    img = loadImage('Bed.webp');
+}
+
+function setup(){
+    canvas =createCanvas(640, 375); 
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects..."; 
+}
+
+function draw(){
+    image(img,0,0,640,420);
+}
+
+function modelLoaded(){
+    console.log("Model loaded!");
+    status = true; 
+    objectDetector.detect(img, gotResult); 
+}
+
+function gotResult(error, results){
+    if (error){
+        console.log(error);
+    }
+    else{
+        console.log(results); 
+    }
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function Bed(){
+    window.location ="bed.html";
+    }
+    function Couch(){
+        window.location ="Couch.html";
+        }
+        function Desk(){
+            window.location ="Desk.html";
+            }
+            function Hanging_Chair(){
+                window.location ="hanging_chair.html";
+                }
+                function Stove(){
+                    window.location ="stove.html";
+                    }
